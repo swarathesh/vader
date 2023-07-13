a6524ef00fc2b4bc0638ec59dcea915d71e7bb8e
chandra swarathesh addanki <swarathesh60@gmail.com>
2023-07-13 18:33:05 -0400

    added event emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name:String = 'vader';
+  choice:String = 'recipe';
+
+  onSelect(feature: string) {
+    this.choice = feature;
+  }
+
 }
